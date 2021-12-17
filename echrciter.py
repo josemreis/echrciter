d1cb0e5a31358cc4c1b1adbd81e482404d20e4a1
josemreis <josemreis@pm.me>
2021-12-17 18:14:58 +0000

    custom error class for case not found

--- echrciter.py
+++ echrciter.py
@@ -23,6 +23,9 @@ TEMPLATE_URL_ECHR_RECORDS = """https://www.echr.coe.int/app/query/results?query=
 TEMPLATE_URL_HUDOC = """https://hudoc.echr.coe.int/app/query/results?query=contentsitename=ECHR {application_number} AND (doctype:CLIN OR doctype:CLINF)&select=sharepointid,Rank,ECHRRanking,languagenumber,itemid,docname,doctype,application,appno,conclusion,importance,originatingbody,typedescription,kpdate,kpdateAsText,documentcollectionid,documentcollectionid2,languageisocode,extractedappno,isplaceholder,doctypebranch,respondent,advopidentifier,advopstatus,ecli,appnoparts,sclappnos&sort=&start=0&length=500"""
 
 
+class NoCases(Exception):
+    pass
+
 def parse_args() -> argparse.Namespace:
     """argument parser"""
 
@@ -95,7 +98,7 @@ def fetch_case_details(app_no: str, max_attempts=5) -> Optional[dict]:
     ## extract the relevant case details
     case_details = {}
     if resp_json.get("resultcount") < 1:
-        raise ValueError("No cases found in hudoc")
+        raise NoCases("No cases found in hudoc")
     for result_dict in resp_json.get("results"):
         lang_dict = result_dict.get("columns")
         if lang_dict.get("appnoparts").replace(";", "/") == app_no:
@@ -177,7 +180,11 @@ def main():
     ## validate the appno
     appno = validate_appno(args.appno.strip())
     ## fetch case detailks
-    case_details_raw = fetch_case_details(app_no=appno)
+    try:  
+        case_details_raw = fetch_case_details(app_no=appno)
+    except NoCases:
+        case_details_raw = None
+        pass
     if not case_details_raw:
         print(f"Case {appno} not found in HUDOC's database")
         sys.exit(1)
